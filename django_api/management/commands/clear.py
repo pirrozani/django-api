439d09901d8f94eaa9fcf3eeaaad24736fa3a561
pirrozani <pirrozani@outlook.com>
2024-02-26 23:41:44 +0200

    refactor(clear.py): Update `clear_data_and_reset_index` method  to clear blog table and add missing `reset_sequence` for reseting the index for all tables

--- django_api/management/commands/clear.py
+++ django_api/management/commands/clear.py
@@ -1,19 +1,32 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.apps import apps
-from api.models import User
-
+from django.db import connection
 
 class Command(BaseCommand):
     help = 'Clear the User table and reset its auto-increment index'
 
+    # Reset the auto-increment index for the given model
+    def reset_sequence(self, model):
+        table_name = model._meta.db_table
+        with connection.cursor() as cursor:
+            cursor.execute(f"DELETE FROM sqlite_sequence WHERE name='{table_name}';")
+
     def handle(self, *args, **options):
         self.clear_data_and_reset_index()
 
+    # Clear the User table and reset its auto-increment index
     def clear_data_and_reset_index(self):
         user_model = apps.get_model('api', 'User')
+        blog_model = apps.get_model('api', 'Blog')
+        
         with transaction.atomic():
             user_model.objects.all().delete()
-            self.stdout.write(self.style.SUCCESS(
-                'Successfully clear the Users table and reset its auto-increment index.')
-            )
+            self.reset_sequence(user_model)
+
+            blog_model.objects.all().delete()
+            self.reset_sequence(blog_model)
+        
+        self.stdout.write(self.style.SUCCESS(
+            'Successfully clear and reset auto-increment index for each table.')
+        )
